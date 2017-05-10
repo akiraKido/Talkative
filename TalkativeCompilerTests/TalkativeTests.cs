269ef3be0fd56d97ef6a2a22a4529c250b3fd5fb
Akira Kido <achts.y@gmail.com>
2017-05-10 19:48:22 +0900

    間違えてテストを削除してしまった

---
+++ TalkativeCompilerTests/TalkativeTests.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TalkativeCompiler;
+
+namespace TalkativeCompilerTests
+{
+    [TestClass()]
+    public class TalkativeTests
+    {
+        [TestMethod()]
+        public void AssignmentOperatorTest ()
+        {
+            var sourceCode = "x := 1.";
+
+            var expected = new List<Token>
+            {
+                new Token( TokenType.Identifier, "x" ),
+                new Token( TokenType.AssignmentOperator, ":=" ),
+                new Token( TokenType.Numeral, "1" )
+            };
+            
+            TryTest( sourceCode, expected );
+        }
+
+        [TestMethod]
+        public void NumberLiteralTests()
+        {
+            var sourceCode = "10";
+            var expected = new List<Token> {new Token( TokenType.Numeral, "10" )};
+            TryTest( sourceCode,expected );
+
+            sourceCode = "-10";
+            expected = new List<Token> {new Token( TokenType.Numeral, "-10" )};
+            TryTest( sourceCode, expected );
+
+            sourceCode = "x := -10";
+            expected = new List<Token>
+            {
+                new Token( TokenType.Identifier, "x" ),
+                new Token( TokenType.AssignmentOperator, ":=" ),
+                new Token( TokenType.Numeral, "-10" )
+            };
+            TryTest( sourceCode, expected );
+        }
+
+        [TestMethod]
+        public void StringLiteralTest()
+        {
+            const string test1 = "'Smalltalk'";
+
+            var expected1 = new List<Token>
+            {
+                new Token( TokenType.StringLiteral, "Smalltalk" )
+            };
+
+            TryTest( test1, expected1 );
+
+            const string test2 = "'I''m a Smalltalker.'";
+            var expected2 = new List<Token>
+            {
+                new Token( TokenType.StringLiteral, "I'm a Smalltalker." )
+            };
+
+            TryTest( test2, expected2 );
+        }
+
+        [TestMethod]
+        public void StringConnectionTest()
+        {
+            const string test = "'Small','talk'";
+            var expected = new List<Token>
+            {
+                new Token( TokenType.StringLiteral, "Smalltalk" )
+            };
+            TryTest( test, expected );
+        }
+
+        [TestMethod]
+        public void CommentOutTest()
+        {
+            const string test = "\"This is a test\"";
+            var expected = new List<Token>();
+            TryTest( test, expected );
+        }
+
+        [TestMethod]
+        public void SymbolTest()
+        {
+            var test = "#hoge";
+            var expected = new List<Token> { new Token( TokenType.Symbol, "hoge" ) };
+            TryTest( test, expected );
+
+            test = "#hoge#fuga";
+            expected = new List<Token>
+            {
+                new Token( TokenType.Symbol, "hoge" ),
+                new Token( TokenType.Symbol, "fuga" ),
+            };
+            TryTest( test, expected );
+
+            test = "#hoge" + Environment.NewLine + "#fuga";
+            expected = new List<Token>
+            {
+                new Token( TokenType.Symbol, "hoge" ),
+                new Token( TokenType.Symbol, "fuga" ),
+            };
+            TryTest( test, expected );
+
+        }
+
+        [TestMethod]
+        public void ArrayTest()
+        {
+            var test = "#(10 5)";
+            var expected = new List<Token>
+            {
+                new Token( TokenType.ArrayStart, "(" ),
+                new Token( TokenType.Numeral, "10" ),
+                new Token( TokenType.Numeral, "5" ),
+                new Token( TokenType.ArrayEnd, ")" ),
+            };
+            TryTest( test, expected );
+
+            test = "#(10 5).";
+            TryTest( test, expected );
+
+            test = "x := #(10 5).";
+            expected = new List<Token>
+            {
+                new Token( TokenType.Identifier, "x" ),
+                new Token( TokenType.AssignmentOperator, ":=" ),
+                new Token( TokenType.ArrayStart, "(" ),
+                new Token( TokenType.Numeral, "10" ),
+                new Token( TokenType.Numeral, "5" ),
+                new Token( TokenType.ArrayEnd, ")" ),
+            };
+            TryTest( test, expected );
+            
+            test = "x := #(10 'SmallTalk').";
+            expected = new List<Token>
+            {
+                new Token( TokenType.Identifier, "x" ),
+                new Token( TokenType.AssignmentOperator, ":=" ),
+                new Token( TokenType.ArrayStart, "(" ),
+                new Token( TokenType.Numeral, "10" ),
+                new Token( TokenType.StringLiteral, "SmallTalk" ),
+                new Token( TokenType.ArrayEnd, ")" ),
+            };
+            TryTest( test, expected );
+
+            test = "x := #(10 'SmallTalk' ).";
+            TryTest( test, expected );
+        }
+
+        [TestMethod]
+        public void TotalTest1()
+        {
+            string test = "\"this is a test\"" + Environment.NewLine +
+                          "x := 10." + Environment.NewLine +
+                          "y := 'Small' , 'Talker'.";
+            var expected = new List<Token>
+            {
+                new Token( TokenType.Identifier, "x" ),
+                new Token( TokenType.AssignmentOperator, ":=" ),
+                new Token( TokenType.Numeral, "10" ),
+                new Token( TokenType.LineBreak, "." ),
+                new Token( TokenType.Identifier, "y" ),
+                new Token( TokenType.AssignmentOperator, ":=" ),
+                new Token( TokenType.StringLiteral, "SmallTalker" )
+            };
+
+            TryTest( test, expected );
+        }
+
+        private void TryTest(string sourceCode, List<Token> expected)
+        {
+            var actual = Talkative.Parse( sourceCode );
+            AssertEqual( expected, actual );
+
+        }
+
+        private void AssertEqual( List<Token> expected, IEnumerable<Token> actual )
+        {
+            Assert.AreEqual( expected.Count, actual.Count() );
+
+            int i = 0;
+            foreach ( Token token in actual )
+            {
+                Debug.WriteLine( $"actual token: {token.Value}" );
+                Assert.AreEqual( expected[i].Type, token.Type );
+                Assert.AreEqual( expected[i].Value, token.Value );
+                i++;
+            }
+        }
+    }
+}
